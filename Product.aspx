4ff3016be68c9340af0aa8d623c60d37da23462e
Hp <Hp@DESKTOP-6SRC66K>
2024-09-13 14:23:50 +0530

    Add project files.

---
+++ Product.aspx
@@ -0,0 +1,112 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Product.aspx.cs" Inherits="ProductManagement.Product" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Product Management</title>
+    <style>
+        form {
+            margin: 0 auto;
+            width: 700px;
+            background-color: #ffffff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+
+        label, input, button {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        label {
+            font-weight: bold;
+        }
+
+        input[type="text"] {
+            padding: 5px;
+            font-size: 1em;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            width: calc(100% - 20px);
+        }
+
+        .search-container {
+            display: flex;
+        }
+
+        .Btn-container {
+            display: flex;
+            justify-content: space-around;
+        }
+
+        .btn-red {
+            background-color: #ff0000;
+            color: white;
+            border: none;
+            border-radius: 5px;
+        }
+
+        .btn-green {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+            .btn-green:hover {
+                background-color: #45a049;
+            }
+
+        .actions {
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="search-container">
+                <asp:TextBox ID="txt_Search" runat="server" placeholder="Search category or username"></asp:TextBox>
+                <asp:Button ID="btn_Search" runat="server" Text="Search" OnClick="btn_Search_Click" CssClass="btn-red" />
+            </div>
+            <br />
+            <asp:Label runat="server" Text="Product Name"></asp:Label>
+            <asp:TextBox ID="txt_ProductName" runat="server" placeholder="Enter product name"></asp:TextBox>
+            <br />
+            <asp:Label runat="server" Text="Product Description"></asp:Label>
+            <asp:TextBox ID="txt_description" runat="server" placeholder="Enter product description"></asp:TextBox>
+            <br />
+            <asp:Label runat="server" Text="Category Name"></asp:Label>
+            <asp:TextBox ID="txt_c_name" runat="server" placeholder="Enter category name"></asp:TextBox>
+            <br />
+            <asp:Label runat="server" Text="User Name"></asp:Label>
+            <asp:TextBox ID="txt_Username" runat="server" placeholder="Enter user name"></asp:TextBox>
+            <br />
+            <div class="grid-container">
+                <asp:GridView ID="GridView1" runat="server" OnRowCommand="GridView1_RowCommand">
+                    <Columns>
+                        <asp:TemplateField HeaderText="Actions">
+                            <ItemTemplate>
+                                <div class="actions">
+                                    <asp:Button ID="btn_Edit" runat="server" Text="Edit" CommandName="EditProduct" CommandArgument='<%# Eval("ProductID") %>' />
+                                    <asp:Button ID="btn_delete" runat="server" Text="Delete" CommandName="DeleteProduct" CommandArgument='<%# Eval("ProductID") %>' />
+                                </div>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+            <br>
+            <div class="Btn-container">
+                <asp:Button ID="btn_Submit" runat="server" Text="Submit" OnClick="btn_Submit_Click" CssClass="btn-green" />
+                <asp:Button ID="btn_Load" runat="server" Text="Load" OnClick="btn_Load_Click" CssClass="btn-green" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Product.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace ProductManagement
+{
+    public partial class Product : System.Web.UI.Page
+    {
+        public SqlConnection con = new SqlConnection("Data Source=DESKTOP-6SRC66K\\SQLEXPRESS;Initial Catalog=Product;Integrated Security=True;TrustServerCertificate=True");
+        public static int productId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                DispalyRecords();
+        }
+        private void DispalyRecords()
+        {
+            SqlCommand cmd = new SqlCommand("FetchData", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Action", "SELECT");
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            productId = Convert.ToInt32(e.CommandArgument);
+            if (e.CommandName == "EditProduct")
+            {
+                SqlCommand cmd = new SqlCommand("Fetchupdate", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@UPDATEID", productId);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    txt_ProductName.Text = dr["ProductName"].ToString();
+                    txt_description.Text = dr["ProductDescription"].ToString();
+                    txt_c_name.Text = dr["CategoryName"].ToString();
+                    txt_Username.Text = dr["Username"].ToString();
+                }
+
+            }
+            else if (e.CommandName == "DeleteProduct")
+            {
+                SqlCommand cmd = new SqlCommand("FetchData", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Action", "DELETE");
+                cmd.Parameters.Add("@UPDATEID", productId);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                DispalyRecords();
+                productId = 0;
+                Clear();
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Deleted Successfully')", true);
+            }
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            string ProductName = txt_ProductName.Text;
+            string ProductDescription = txt_description.Text;
+            string CategoryName = txt_c_name.Text;
+            string Username = txt_Username.Text;
+            if (Validatetxt())
+            {
+                if (productId != 0)
+                {
+                    SqlCommand cmd = new SqlCommand("FetchData", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@Action", "UPDATE");
+                    cmd.Parameters.Add("@UPDATEID", productId);
+                    cmd.Parameters.Add("@ProductName", ProductName);
+                    cmd.Parameters.Add("@ProductDescription", ProductDescription);
+                    cmd.Parameters.Add("@CategoryName", CategoryName);
+                    cmd.Parameters.Add("@UserName", Username);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    DispalyRecords();
+                    productId = 0;
+                    Clear();
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Updated Successfully')", true);
+                }
+                else
+                {
+                    SqlCommand cmd = new SqlCommand("FetchData", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@Action", "INSERT");
+                    cmd.Parameters.Add("@ProductName", ProductName);
+                    cmd.Parameters.Add("@ProductDescription", ProductDescription);
+                    cmd.Parameters.Add("@CategoryName", CategoryName);
+                    cmd.Parameters.Add("@UserName", Username);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    DispalyRecords();
+                    Clear();
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Please Enter All Details')", true);
+            }
+        }
+        protected void btn_Search_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txt_Search.Text))
+            {
+                string Search = txt_Search.Text;
+                SqlCommand cmd = new SqlCommand("SearchProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Username_Category", Search);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Please Enter Username Or Category')", true);
+
+            }
+        }
+        private void Clear()
+        {
+            txt_ProductName.Text = string.Empty;
+            txt_description.Text = string.Empty;
+            txt_c_name.Text = string.Empty;
+            txt_Username.Text = string.Empty;
+        }
+
+        protected void btn_Load_Click(object sender, EventArgs e)
+        {
+            DispalyRecords();
+        }
+        private bool Validatetxt()
+        {
+            if (!string.IsNullOrEmpty(txt_ProductName.Text) && !string.IsNullOrEmpty(txt_description.Text) &&
+                   !string.IsNullOrEmpty(txt_c_name.Text) && !string.IsNullOrEmpty(txt_Username.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
